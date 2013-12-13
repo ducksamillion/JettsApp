57d732cd816093adf253bd1c38c0ea41b8fc145b
ducksamillion <account.temp.c@gmail.com>
2013-12-14 02:45:16 +1030

    first commit

---
+++ data/db.js
@@ -0,0 +1,25 @@
+﻿/// <reference path="../js/jquery-1.10.2.min.js"; />
+/// <reference path="../js/knockout-3.0.0.js"; />
+
+(function() {
+
+    var products = ["Valley View", "St Morris", "St Marys", "Kidman Park", "Newton"];
+	
+    var productsCustom = [
+        { Name: products[0], Location: "901 Grand Junction Rd, Valley View SA 5093", Image: "content/images/0.png" },
+        { Name: products[1], Location: "Ground Floor, 427 Magill Rd, St Morris SA 5068", Image: "content/images/0.png" },
+        { Name: products[2], Location: "Tenancy 3/1223-1231 S Rd, St Marys SA 5042", Image: "content/images/0.png" },
+        { Name: products[3], Location: "308-320 Grange Rd, Kidman Park SA 5025", Image: "content/images/0.png" },
+        { Name: products[4], Location: "299 Montacute Rd, Newton SA 5074", Image: "content/images/0.png" },
+    ];
+    var gallery = [
+		{ src: "content/images/gallery/0.jpg" },
+		{ src: "content/images/gallery/1.jpg" },
+		{ src: "content/images/gallery/2.jpg" },
+    ];
+    JettsApp.db = {
+        products: new DevExpress.data.ArrayStore(products),
+        productsCustom: new DevExpress.data.ArrayStore(productsCustom),
+        gallery: new DevExpress.data.ArrayStore(gallery),
+    };
+})();
---
+++ index.js
@@ -0,0 +1,55 @@
+﻿(function() {
+    var JettsApp = window.JettsApp = { };
+    
+    // Uncomment the line below to disable platform-specific look and feel and to use the Generic theme for all devices
+    // DevExpress.devices.current({ platform: "generic" });
+
+    $(function() {
+        JettsApp.app = new DevExpress.framework.html.HtmlApplication({
+            namespace: JettsApp,
+            
+            navigationType: "navbar",
+            navigation: [
+              {
+                title: "Home",
+                action: "#home",
+                icon: "home"
+              },
+              {
+                title: "Traffic",
+                action: "#traffic",
+                icon: "info"
+              },
+              {
+                title: "Observe",
+                action: "#observe",
+                icon: "info"
+              }
+            ]
+        });
+        
+        JettsApp.app.router.register(":view", { view: "home" });
+        JettsApp.app.navigate();
+    });
+	
+	JettsApp.observe = function(params) {
+	    return {
+			data: JettsApp.db.gallery
+		};
+	};
+	
+	JettsApp.home = function (params) {
+		function handleItemClick(e) {
+			JettsApp.app.navigate("traffic");
+		};
+
+		return {
+            dataSource: new DevExpress.data.DataSource({ store: JettsApp.db.productsCustom }),
+			handleItemClick: handleItemClick,
+
+			viewShown: function() {
+				$(".dx-active-view .dx-scrollable").data("dxScrollView").scrollTo(0);
+			}
+		};
+	};
+})();
